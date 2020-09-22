54a252e788203de1583de4557424ec03124c2c12
Hussain Feroz <hussainferoz839@gmail.com>
2020-09-22 23:46:45 +0500

    basic layout

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import {View, StatusBar, StyleSheet} from 'react-native';
+import {View, StatusBar, StyleSheet, Dimensions} from 'react-native';
+
+const {width, height} = Dimensions.get('window');
 
 const App = () => {
   return (
     <View style={styles.container}>
       <StatusBar barStyle="light-content" />
+      <View style={styles.cardContainer}>
+        <View style={styles.card}></View>
+      </View>
+      <View style={styles.formContainer}></View>
     </View>
   );
 };
@@ -12,6 +18,25 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#2b92e4',
+  },
+  cardContainer: {
+    flex: 2,
+    backgroundColor: '#2b92e4',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  card: {
+    width: width - 60,
+    height: height / 3.5,
+    borderRadius: 20,
+    backgroundColor: '#f8b5c1',
+  },
+  formContainer: {
+    flex: 3,
+    backgroundColor: '#fff',
+    borderTopLeftRadius: 35,
+    borderTopRightRadius: 35,
   },
 });
 
