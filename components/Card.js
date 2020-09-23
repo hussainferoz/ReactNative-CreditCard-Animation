2120fe2fede7e78a41110936abe172195651eb50
Hussain Feroz <hussainferoz839@gmail.com>
2020-09-23 23:46:14 +0500

    card content added

--- components/Card.js
+++ components/Card.js
@@ -1,13 +1,22 @@
 import React from 'react';
 
-import {StyleSheet, View, Dimensions} from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
+import {StyleSheet, View, Text, Dimensions} from 'react-native';
 
 const {width, height} = Dimensions.get('window');
 
 const Card = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.card}></View>
+      <View style={styles.card}>
+        <FontAwesome name="cc-visa" size={45} color="#000" />
+        <Text style={styles.text}>{`3534    3500    0000    3654`}</Text>
+        <View style={styles.view}>
+          <Text style={styles.text}>Hussain Feroz</Text>
+          <Text style={styles.text}>03/21</Text>
+        </View>
+      </View>
     </View>
   );
 };
@@ -22,7 +31,13 @@ const styles = StyleSheet.create({
   card: {
     borderRadius: 20,
     elevation: 5,
+    padding: 20,
     backgroundColor: '#f8b5c1',
     ...StyleSheet.absoluteFillObject,
+    justifyContent: 'space-between',
   },
+  text: {
+    fontSize: 22,
+  },
+  view: {flexDirection: 'row', justifyContent: 'space-between'},
 });
